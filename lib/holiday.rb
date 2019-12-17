a5e2b72f956574d5b10ed2e4889a1a8a3ab87ed1
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 02:09:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -115,7 +115,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_name, supplies|
     puts season.to_s
-
+  end
 end
 
 # iterate through holiday_hash and print items such that your readout is:
