a8bc551065762df35578e3ba8b1c64f3d8718d8d
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 02:22:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,7 +116,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_name|
     puts season.to_s.capitalize! + ":"
       holiday_name.each do |holiday, value|
-        puts holiday.to_s
+        puts holiday.to_s.capitalize!
+          
       end  
   end
 end
