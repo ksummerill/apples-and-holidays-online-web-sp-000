c872e294098760347b86672e755882737fc30f40
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 02:37:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -116,8 +116,15 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_name|
     puts season.to_s.capitalize! + ":"
       holiday_name.each do |holiday, value|
+        if holiday_name
         puts "  " + holiday.to_s.capitalize! + ": " + value.join(", ")
-
+      else holiday_name == :new_years || holiday_name == :fourth_of_july || holiday_name == :memorial_day
+        capitalize_holiday_array = holiday_name.to_s.split
+        # [new, years]
+        capitalize_holiday_array.each do |cap|
+          cap.capitalize!.join()
+        puts "  " + 
+      end
       end
   end
 end
