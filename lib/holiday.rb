18755f930f8c06e31faa6197597c4b8378b3cf1b
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 03:05:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -118,7 +118,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |holiday, value|
-      holiday if value.include?("BBQ")
+      return holiday if value.include?("BBQ")
     end
   end
   
